74eba2a6dccbcee0462784ad9765cdfe32285148
Lorenza <ls005515@cfjl.com.br>
2018-06-28 22:02:50 -0300

    Adicionado arquivos de teste

---
+++ E-commerce/E-commerce-ejb/src/java/model/Administrador.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Loren
+ */
+public class Administrador {
+
+    private int CADMIN;
+    private String LOGIN;
+    private String SENHA;
+
+    public int getCADMIN() {
+        return CADMIN;
+    }
+
+    public void setCADMIN(int CADMIN) {
+        this.CADMIN = CADMIN;
+    }
+
+    public String getLOGIN() {
+        return LOGIN;
+    }
+
+    public void setLOGIN(String LOGIN) {
+        this.LOGIN = LOGIN;
+    }
+
+    public String getSENHA() {
+        return SENHA;
+    }
+
+    public void setSENHA(String SENHA) {
+        this.SENHA = SENHA;
+    }
+}
---
+++ E-commerce/E-commerce-ejb/src/java/model/Estado.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Loren
+ */
+public class Estado {
+
+    private String UF;
+    private String DESCRICAO;
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public String getDESCRICAO() {
+        return DESCRICAO;
+    }
+
+    public void setDESCRICAO(String DESCRICAO) {
+        this.DESCRICAO = DESCRICAO;
+    }
+}
