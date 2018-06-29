9ba27dbde217437f15688a9bbb63a7b5dc3bbbb7
Lorenza <ls005515@cfjl.com.br>
2018-06-28 21:12:46 -0300

    Adicionado arquivos de teste

---
+++ E-commerce/E-commerce-ejb/src/model/Produto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Loren
+ */
+public class Produto {
+
+    private int CPRODUTO;
+    private String PRODUTO;
+    private String DESCRICAO;
+    private byte[] IMAGEM;
+}
---
+++ E-commerce/E-commerce-ejb/test/model/Teste.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Loren
+ */
+@Stateless
+public class Teste implements TesteRemote, TesteLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
